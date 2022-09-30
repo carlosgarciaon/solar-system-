a188dff10bd5a27395c11417e53873c82feba3c8
Carlos García <>
2022-09-30 15:02:05 -0500

    Summary

---
+++ main.js
@@ -0,0 +1,96 @@
+window.sr = ScrollReveal();
+
+sr.reveal('#titulo',{
+    duration: 1000,
+    origin: 'top',
+    distance: '100px'
+});
+
+sr.reveal('.sol',{
+    duration: 1000,
+    scale: 0.5
+});
+
+sr.reveal('#mer',{
+    delay: 100,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#ven',{
+    delay: 150,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#tie',{
+    delay: 200,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#mar',{
+    delay: 250,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#jup',{
+    delay: 300,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#sat',{
+    delay: 350,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#ura',{
+    delay: 400,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#nep',{
+    delay: 450,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+
+sr.reveal('.planet',{
+    delay: 200,
+    duration: 1000,
+    scale: 0.3
+});
+
+sr.reveal('h2',{
+    delay: 100,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('p',{
+    delay: 200,
+    duration: 1000,
+    origin: 'bottom',
+    distance: '50px'
+});
+
+sr.reveal('button',{
+    delay: 300,
+    duration: 1000,
+    origin: 'bottom',
+    
+});
